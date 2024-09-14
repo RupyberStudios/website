15408893fd77d500f66443bd372c55a1adb98c8e
Pyrix25633 <biral.mattia@gmail.com>
2024-09-14 11:26:09 +0200

    Added Explore Decorate Create to Projects

--- js/projects.js
+++ js/projects.js
@@ -142,48 +142,54 @@ class Project {
         this.project.appendChild(descriptionSpan);
         const linksContainer = document.createElement('div');
         linksContainer.classList.add('container', 'links');
-        const github = document.createElement('div');
-        github.classList.add('container', 'link', 'clickable', 'github');
-        github.addEventListener('click', () => {
-            window.open('https://github.com/Pyrix25633/' + links.github + '/releases');
-        });
-        const githubIcon = document.createElement('img');
-        githubIcon.classList.add('icon');
-        githubIcon.alt = 'GitHub Icon';
-        githubIcon.src = '../../img/github.svg';
-        const githubSpan = document.createElement('span');
-        githubSpan.innerText = 'GitHub';
-        github.appendChild(githubIcon);
-        github.appendChild(githubSpan);
-        const curseforge = document.createElement('div');
-        curseforge.classList.add('container', 'link', 'clickable', 'curseforge');
-        curseforge.addEventListener('click', () => {
-            window.open('https://curseforge.com/minecraft/mc-mods/' + links.curseforge);
-        });
-        const curseforgeIcon = document.createElement('img');
-        curseforgeIcon.classList.add('icon');
-        curseforgeIcon.alt = 'CurseForge Icon';
-        curseforgeIcon.src = '../../img/curseforge.svg';
-        const curseforgeSpan = document.createElement('span');
-        curseforgeSpan.innerText = 'CurseForge';
-        curseforge.appendChild(curseforgeIcon);
-        curseforge.appendChild(curseforgeSpan);
-        const modrinth = document.createElement('div');
-        modrinth.classList.add('container', 'link', 'clickable', 'modrinth');
-        modrinth.addEventListener('click', () => {
-            window.open('https://modrinth.com/mod/' + links.modrinth);
-        });
-        const modrinthIcon = document.createElement('img');
-        modrinthIcon.classList.add('icon');
-        modrinthIcon.alt = 'Modrinth Icon';
-        modrinthIcon.src = '../../img/modrinth.svg';
-        const modrinthSpan = document.createElement('span');
-        modrinthSpan.innerText = 'Modrinth';
-        modrinth.appendChild(modrinthIcon);
-        modrinth.appendChild(modrinthSpan);
-        linksContainer.appendChild(github);
-        linksContainer.appendChild(curseforge);
-        linksContainer.appendChild(modrinth);
+        if(links.github != undefined) {
+            const github = document.createElement('div');
+            github.classList.add('container', 'link', 'clickable', 'github');
+            github.addEventListener('click', () => {
+                window.open('https://github.com/Pyrix25633/' + links.github + '/releases');
+            });
+            const githubIcon = document.createElement('img');
+            githubIcon.classList.add('icon');
+            githubIcon.alt = 'GitHub Icon';
+            githubIcon.src = '../../img/github.svg';
+            const githubSpan = document.createElement('span');
+            githubSpan.innerText = 'GitHub';
+            github.appendChild(githubIcon);
+            github.appendChild(githubSpan);
+            linksContainer.appendChild(github);
+        }
+        if(links.curseforge != undefined) {
+            const curseforge = document.createElement('div');
+            curseforge.classList.add('container', 'link', 'clickable', 'curseforge');
+            curseforge.addEventListener('click', () => {
+                window.open('https://curseforge.com/minecraft/mc-mods/' + links.curseforge);
+            });
+            const curseforgeIcon = document.createElement('img');
+            curseforgeIcon.classList.add('icon');
+            curseforgeIcon.alt = 'CurseForge Icon';
+            curseforgeIcon.src = '../../img/curseforge.svg';
+            const curseforgeSpan = document.createElement('span');
+            curseforgeSpan.innerText = 'CurseForge';
+            curseforge.appendChild(curseforgeIcon);
+            curseforge.appendChild(curseforgeSpan);
+            linksContainer.appendChild(curseforge);
+        }
+        if(links.modrinth != undefined) {
+            const modrinth = document.createElement('div');
+            modrinth.classList.add('container', 'link', 'clickable', 'modrinth');
+            modrinth.addEventListener('click', () => {
+                window.open('https://modrinth.com/mod/' + links.modrinth);
+            });
+            const modrinthIcon = document.createElement('img');
+            modrinthIcon.classList.add('icon');
+            modrinthIcon.alt = 'Modrinth Icon';
+            modrinthIcon.src = '../../img/modrinth.svg';
+            const modrinthSpan = document.createElement('span');
+            modrinthSpan.innerText = 'Modrinth';
+            modrinth.appendChild(modrinthIcon);
+            modrinth.appendChild(modrinthSpan);
+            linksContainer.appendChild(modrinth);
+        }
         this.project.appendChild(linksContainer);
         projects.push(this);
     }
@@ -201,29 +207,34 @@ class Project {
     }
 }
 
-const improvedEnd = new Project('improved_end', [fabric, quilt, _1_19_2], [_1_18_2],
-    'Adds tons of new Blocks, Biomes and Mobs to the End Dimension.',
-    { github: 'Fabric-ImprovedEnd', curseforge: 'improved-end', modrinth: 'improved-end' }
-);
+
 const vanillaPlus = new Project('vanilla_plus', [fabric, quilt, _1_20_4], [_1_18_2, _1_19_4],
     'Adds Decoration Blocks and new Music Discs easy to obtain in survival.',
     { github: 'Fabric-VanillaPlus', curseforge: 'vanilla-plus-mod', modrinth: 'vanilla-plus-mod' }
-);
-const minecraftLegendsMod = new Project('minecraft_legends_mod', [fabric, quilt, _1_19_2], [],
-    'Adds add some Mobs, Structures and Items from Minecraft Legends.',
-    { github: 'Fabric-MinecraftLegends', curseforge: 'minecraft-legends-mod', modrinth: 'minecraft-legends-mod' }
 );
 const fbiAndSwatArmors = new Project('fbi_and_swat_armors', [forge, fabric, quilt, _1_21_1], [_1_19_4, _1_20_4, _1_20_6],
     'Adds FBI and SWAT armors.',
     { github: 'Fabric-Forge-FbiAndSwatArmors', curseforge: 'fbi-and-swat-armors', modrinth: 'fbi-and-swat-armors' }
 );
+const fabricTps = new Project('fabric_tps', [fabric, quilt, _1_21_1], [_1_18_2, _1_19_4, _1_20_6],
+    'Adds an equivalent to the \'/forge tps\' command.',
+    { github: 'Fabric-FabricTPS', curseforge: 'fabric-tps', modrinth: 'fabric-tps' }
+);
+const exploreDecorateCreate = new Project('explore_decorate_create', [fabric, quilt, _1_20_4], [_1_20_1],
+    'Modpack designed to improve Exploration, Decoration and Creation.',
+    { curseforge: 'explore-decorate-create', modrinth: 'explore-decorate-create' }
+);
 const starWarsCloneWars = new Project('star_wars_clone_wars', [forge, _1_20_1], [_1_18_2, _1_19_4],
     'Adds Clones, Battle Droids, Dimensions and much more.',
     { github: 'Forge-StarWarsCloneWars', curseforge: 'star-wars-clone-wars', modrinth: 'star-wars-clone-wars' }
 );
-const fabricTps = new Project('fabric_tps', [fabric, quilt, _1_21_1], [_1_18_2, _1_19_4, _1_20_6],
-    'Adds an equivalent to the \'/forge tps\' command.',
-    { github: 'Fabric-FabricTPS', curseforge: 'fabric-tps', modrinth: 'fabric-tps' }
+const improvedEnd = new Project('improved_end', [fabric, quilt, _1_19_2], [_1_18_2],
+    'Adds tons of new Blocks, Biomes and Mobs to the End Dimension.',
+    { github: 'Fabric-ImprovedEnd', curseforge: 'improved-end', modrinth: 'improved-end' }
+);
+const minecraftLegendsMod = new Project('minecraft_legends_mod', [fabric, quilt, _1_19_2], [],
+    'Adds add some Mobs, Structures and Items from Minecraft Legends.',
+    { github: 'Fabric-MinecraftLegends', curseforge: 'minecraft-legends-mod', modrinth: 'minecraft-legends-mod' }
 );
 
 const main = new Main([new Filter()].concat(projects));
